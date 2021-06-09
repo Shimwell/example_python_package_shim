f3ef9e52ebe60adab2156a6e90ba4e3fd3357c24
Jonathan Shimwell <drshimwell@gmail.com>
2021-06-09 12:06:57 +0100

    added package

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup
+
+setup(
+    name='example_python_package_shim',
+    author='Jon',
+    author_email='mail@jshimwell.com',
+    license='MIT',
+    url='https://github.com/Shimwell/example_python_package_shim',
+    description='XXX Skeleton python project example.',
+    packages=["example_python_package_shim"],
+)
